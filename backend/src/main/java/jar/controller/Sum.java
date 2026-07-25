6cbd90851749d284298736312ac8cc23bc3a48e3
WobblyJoe <24eg107e49@anurag.edu.in>
2026-07-25 15:06:13 +0530

    Implement /api/fact and /api/sum endpoints

--- backend/src/main/java/jar/controller/Sum.java
+++ backend/src/main/java/jar/controller/Sum.java
@@ -1,5 +1,16 @@
 package jar.controller;
 
+import jar.dto.SumDto;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Sum {
-    
+
+    @PostMapping("/api/sum/{a}/{b}")
+    public SumDto getSum(@PathVariable int a, @PathVariable int b) {
+        int result = a + b;
+        return new SumDto("sum api", 200, a, b, result);
+    }
 }
